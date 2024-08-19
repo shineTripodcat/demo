5a4baaa3646ad82214498f1d7907cbda10234db3
shineTripodcat <111100023+shineTripodcat@users.noreply.github.com>
2024-08-20 00:17:09 +0800

    Update xrayL.sh

--- xrayL.sh
+++ xrayL.sh
@@ -51,63 +51,80 @@ config_xray() {
         exit 1
     fi
 
-    # 用户输入起始端口和代理池数量
-    read -p "起始端口 (默认 $DEFAULT_START_PORT): " START_PORT
-    START_PORT=${START_PORT:-$DEFAULT_START_PORT}
-    read -p "代理池 IP 数量: " IP_COUNT
-
     # 初始化配置内容
     config_content=""
 
-    for ((i = 0; i < IP_COUNT; i++)); do
-        if [ "$config_type" == "socks" ]; then
-            read -p "SOCKS 账号 (默认 $DEFAULT_SOCKS_USERNAME): " SOCKS_USERNAME
-            SOCKS_USERNAME=${SOCKS_USERNAME:-$DEFAULT_SOCKS_USERNAME}
+    while true; do
+        # 用户输入起始端口和代理池数量
+        while true; do
+            read -p "起始端口 (默认 $DEFAULT_START_PORT): " START_PORT
+            START_PORT=${START_PORT:-$DEFAULT_START_PORT}
+            
+            if ! lsof -i:"$START_PORT" > /dev/null; then
+                break
+            else
+                echo "端口 $START_PORT 已被占用，请选择其他端口。"
+            fi
+        done
+        
+        read -p "代理池 IP 数量: " IP_COUNT
 
-            read -p "SOCKS 密码 (默认 $DEFAULT_SOCKS_PASSWORD): " SOCKS_PASSWORD
-            SOCKS_PASSWORD=${SOCKS_PASSWORD:-$DEFAULT_SOCKS_PASSWORD}
+        for ((i = 0; i < IP_COUNT; i++)); do
+            if [ "$config_type" == "socks" ]; then
+                read -p "SOCKS 账号 (默认 $DEFAULT_SOCKS_USERNAME): " SOCKS_USERNAME
+                SOCKS_USERNAME=${SOCKS_USERNAME:-$DEFAULT_SOCKS_USERNAME}
 
-            # SOCKS 配置
-            config_content+="[[inbounds]]\n"
-            config_content+="port = $((START_PORT + i))\n"
-            config_content+="protocol = \"socks\"\n"
-            config_content+="tag = \"tag_$((i + 1))\"\n"
-            config_content+="[inbounds.settings]\n"
-            config_content+="auth = \"password\"\n"
-            config_content+="udp = true\n"
-            config_content+="[[inbounds.settings.accounts]]\n"
-            config_content+="user = \"$SOCKS_USERNAME\"\n"
-            config_content+="pass = \"$SOCKS_PASSWORD\"\n\n"
+                read -p "SOCKS 密码 (默认 $DEFAULT_SOCKS_PASSWORD): " SOCKS_PASSWORD
+                SOCKS_PASSWORD=${SOCKS_PASSWORD:-$DEFAULT_SOCKS_PASSWORD}
 
-        elif [ "$config_type" == "vmess" ]; then
-            read -p "UUID (默认随机): " UUID
-            UUID=${UUID:-$DEFAULT_UUID}
-            read -p "WebSocket 路径 (默认 $DEFAULT_WS_PATH): " WS_PATH
-            WS_PATH=${WS_PATH:-$DEFAULT_WS_PATH}
+                # SOCKS 配置
+                config_content+="[[inbounds]]\n"
+                config_content+="port = $((START_PORT + i))\n"
+                config_content+="protocol = \"socks\"\n"
+                config_content+="tag = \"tag_$((i + 1))\"\n"
+                config_content+="[inbounds.settings]\n"
+                config_content+="auth = \"password\"\n"
+                config_content+="udp = true\n"
+                config_content+="[[inbounds.settings.accounts]]\n"
+                config_content+="user = \"$SOCKS_USERNAME\"\n"
+                config_content+="pass = \"$SOCKS_PASSWORD\"\n\n"
 
-            # VMess 配置
-            config_content+="[[inbounds]]\n"
-            config_content+="port = $((START_PORT + i))\n"
-            config_content+="protocol = \"vmess\"\n"
-            config_content+="tag = \"tag_$((i + 1))\"\n"
-            config_content+="[inbounds.settings]\n"
-            config_content+="[[inbounds.settings.clients]]\n"
-            config_content+="id = \"$UUID\"\n"
-            config_content+="[inbounds.streamSettings]\n"
-            config_content+="network = \"ws\"\n"
-            config_content+="[inbounds.streamSettings.wsSettings]\n"
-            config_content+="path = \"$WS_PATH\"\n\n"
+            elif [ "$config_type" == "vmess" ]; then
+                read -p "UUID (默认随机): " UUID
+                UUID=${UUID:-$DEFAULT_UUID}
+                read -p "WebSocket 路径 (默认 $DEFAULT_WS_PATH): " WS_PATH
+                WS_PATH=${WS_PATH:-$DEFAULT_WS_PATH}
+
+                # VMess 配置
+                config_content+="[[inbounds]]\n"
+                config_content+="port = $((START_PORT + i))\n"
+                config_content+="protocol = \"vmess\"\n"
+                config_content+="tag = \"tag_$((i + 1))\"\n"
+                config_content+="[inbounds.settings]\n"
+                config_content+="[[inbounds.settings.clients]]\n"
+                config_content+="id = \"$UUID\"\n"
+                config_content+="[inbounds.streamSettings]\n"
+                config_content+="network = \"ws\"\n"
+                config_content+="[inbounds.streamSettings.wsSettings]\n"
+                config_content+="path = \"$WS_PATH\"\n\n"
+            fi
+
+            # 公共配置部分
+            config_content+="sendThrough = \"${IP_ADDRESSES[0]}\"\n"
+            config_content+="protocol = \"freedom\"\n"
+            config_content+="tag = \"tag_$((i + 1))\"\n\n"
+
+            config_content+="[[routing.rules]]\n"
+            config_content+="type = \"field\"\n"
+            config_content+="inboundTag = \"tag_$((i + 1))\"\n"
+            config_content+="outboundTag = \"tag_$((i + 1))\"\n\n\n"
+        done
+
+        # 检查是否继续添加用户
+        read -p "是否继续添加用户？(y/n): " add_more
+        if [ "$add_more" != "y" ]; then
+            break
         fi
-
-        # 公共配置部分
-        config_content+="sendThrough = \"${IP_ADDRESSES[0]}\"\n" # 使用第一个 IP 地址
-        config_content+="protocol = \"freedom\"\n"
-        config_content+="tag = \"tag_$((i + 1))\"\n\n"
-
-        config_content+="[[routing.rules]]\n"
-        config_content+="type = \"field\"\n"
-        config_content+="inboundTag = \"tag_$((i + 1))\"\n"
-        config_content+="outboundTag = \"tag_$((i + 1))\"\n\n\n"
     done
 
     # 根据选择的类型保存配置到相应文件
@@ -123,89 +140,6 @@ config_xray() {
     systemctl --no-pager status xrayL.service
     echo ""
     echo "生成 $config_type 配置完成"
-    echo "配置文件保存为: $CONFIG_FILE"
-    echo "起始端口: $START_PORT"
-    echo "结束端口: $((START_PORT + IP_COUNT - 1))"
-    
-    if [ "$config_type" == "socks" ]; then
-        echo "SOCKS 账号: $SOCKS_USERNAME"
-        echo "SOCKS 密码: $SOCKS_PASSWORD"
-    elif [ "$config_type" == "vmess" ]; then
-        echo "UUID: $UUID"
-        echo "WebSocket 路径: $WS_PATH"
-    fi
-    echo ""
-}
-
-# 增加用户配置
-add_user() {
-    config_type=$1
-    read -p "起始端口 (默认 $DEFAULT_START_PORT): " START_PORT
-    START_PORT=${START_PORT:-$DEFAULT_START_PORT}
-    read -p "代理池 IP 数量: " IP_COUNT
-
-    user_config=""
-    for ((i = 0; i < IP_COUNT; i++)); do
-        if [ "$config_type" == "socks" ]; then
-            read -p "SOCKS 账号 (默认 $DEFAULT_SOCKS_USERNAME): " SOCKS_USERNAME
-            SOCKS_USERNAME=${SOCKS_USERNAME:-$DEFAULT_SOCKS_USERNAME}
-
-            read -p "SOCKS 密码 (默认 $DEFAULT_SOCKS_PASSWORD): " SOCKS_PASSWORD
-            SOCKS_PASSWORD=${SOCKS_PASSWORD:-$DEFAULT_SOCKS_PASSWORD}
-
-            user_config+="[[inbounds]]\n"
-            user_config+="port = $((START_PORT + i))\n"
-            user_config+="protocol = \"socks\"\n"
-            user_config+="tag = \"tag_$((i + 1))\"\n"
-            user_config+="[inbounds.settings]\n"
-            user_config+="auth = \"password\"\n"
-            user_config+="udp = true\n"
-            user_config+="[[inbounds.settings.accounts]]\n"
-            user_config+="user = \"$SOCKS_USERNAME\"\n"
-            user_config+="pass = \"$SOCKS_PASSWORD\"\n\n"
-
-        elif [ "$config_type" == "vmess" ]; then
-            read -p "UUID (默认随机): " UUID
-            UUID=${UUID:-$DEFAULT_UUID}
-            read -p "WebSocket 路径 (默认 $DEFAULT_WS_PATH): " WS_PATH
-            WS_PATH=${WS_PATH:-$DEFAULT_WS_PATH}
-
-            user_config+="[[inbounds]]\n"
-            user_config+="port = $((START_PORT + i))\n"
-            user_config+="protocol = \"vmess\"\n"
-            user_config+="tag = \"tag_$((i + 1))\"\n"
-            user_config+="[inbounds.settings]\n"
-            user_config+="[[inbounds.settings.clients]]\n"
-            user_config+="id = \"$UUID\"\n"
-            user_config+="[inbounds.streamSettings]\n"
-            user_config+="network = \"ws\"\n"
-            user_config+="[inbounds.streamSettings.wsSettings]\n"
-            user_config+="path = \"$WS_PATH\"\n\n"
-        fi
-
-        user_config+="sendThrough = \"${IP_ADDRESSES[0]}\"\n"
-        user_config+="protocol = \"freedom\"\n"
-        user_config+="tag = \"tag_$((i + 1))\"\n\n"
-
-        user_config+="[[routing.rules]]\n"
-        user_config+="type = \"field\"\n"
-        user_config+="inboundTag = \"tag_$((i + 1))\"\n"
-        user_config+="outboundTag = \"tag_$((i + 1))\"\n\n\n"
-    done
-
-    # 根据选择的类型保存配置到相应文件
-    if [ "$config_type" == "socks" ]; then
-        CONFIG_FILE="/etc/xrayL/socks5_config.toml"
-    elif [ "$config_type" == "vmess" ]; then
-        CONFIG_FILE="/etc/xrayL/vmess_config.toml"
-    fi
-
-    # 追加用户配置到文件
-    echo -e "$user_config" >> "$CONFIG_FILE"
-    systemctl restart xrayL.service
-    systemctl --no-pager status xrayL.service
-    echo ""
-    echo "添加 $config_type 用户配置完成"
     echo "配置文件保存为: $CONFIG_FILE"
     echo "起始端口: $START_PORT"
     echo "结束端口: $((START_PORT + IP_COUNT - 1))"
@@ -241,17 +175,8 @@ main() {
         echo "未正确选择类型，使用默认 SOCKS 配置."
         config_xray "socks"
     fi
-
-    # 增加用户配置
-    while true; do
-        read -p "是否添加更多用户配置？(y/n): " add_more
-        if [ "$add_more" == "y" ]; then
-            add_user "$config_type"
-        else
-            break
-        fi
-    done
 }
 
 # 执行主函数
 main "$@"
+
